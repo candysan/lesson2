c2eb722b7a7e412d066666cdd96ed185697001bd
Candy <candychama@gmail.com>
2016-05-08 22:57:56 +0900

    add review

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+ devise_for :users
   resources :notes
   resources :users, only: [:show, :new]
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show, :new, :create]
+  resources :reviews, only: [:index, :new]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508122740) do
+ActiveRecord::Schema.define(version: 20160508131716) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20160508122740) do
     t.text     "amazon_url",      limit: 65535
     t.text     "image",           limit: 65535
     t.text     "text",            limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "username",   limit: 255
+    t.integer  "rate",       limit: 4
+    t.text     "review",     limit: 65535
+    t.integer  "product_id", limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
   end
