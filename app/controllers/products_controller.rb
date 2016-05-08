b3adfdd6f7e9226dc915f428d5dab2821f0369dd
Candy <candychama@gmail.com>
2016-05-08 14:32:56 +0900

    install devise view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,14 @@
 class ProductsController < ApplicationController
+	before_action :move_to_index, except: :index
+
 	def index
 		@products = Product.all
 	end
+
+	private
+    def move_to_index
+      redirect_to action: :index unless user_signed_in?
+      # indexアクションを強制的に実行する
+    end
+
 end
